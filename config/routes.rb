5dc0176fcd05ae98dd9d95338a7f1fe821405d37
Hugo <huwgoma@gmail.com>
2022-09-17 18:06:59 -0400

    Fix routing issue after deleting a user; change flash message for successful deletion of user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "events#index"
+
+  devise_scope :user do 
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
 end
