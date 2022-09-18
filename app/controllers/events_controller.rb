6c466fdf14418da1d943e5f2ba51b7a772e6bf2c
Hugo <huwgoma@gmail.com>
2022-09-18 13:03:55 -0400

    Add skeleton for creating events - create new/create/show routes; create actions in EventsController; update header to show link for creating events (if logged in); require user to be logged in to access the new/create actions for Events; add validation to Events to require name, datetime, and location's presence; and create view files for new and show events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,19 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+  
+  end
+
+  def show
+
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
+  validates :name, :datetime, :location, presence: true 
   belongs_to :host, class_name: "User"
 end
