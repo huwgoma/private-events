52c97e3f8dfdee9fdd251afca0112f03b632dd0b
Hugo <huwgoma@gmail.com>
2022-09-18 15:36:51 -0400

    Write Events#create method - Build the event onto the current user's collection of hosted events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,10 +10,22 @@ class EventsController < ApplicationController
   end
 
   def create
-  
+    @event = current_user.hosted_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
 
+  end
+
+  private 
+
+  def event_params
+    params.require(:event).permit([:name, :date_of, :time_of, :location, :description])
   end
 end
