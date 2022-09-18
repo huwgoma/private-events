bde6cf6ee61022c5aaf558b3c5f9c98bf5409a3a
Hugo <huwgoma@gmail.com>
2022-09-18 16:54:12 -0400

    Add helper methods in Event Model for formatting Event date/times; update views accordingly

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ApplicationRecord
   validates :name, :datetime_of, :location, presence: true 
   belongs_to :host, class_name: "User"
+
+  # Format Event Date/Times
+  def format_date
+    datetime_of.strftime("%B %d, %Y")
+  end
+
+  def format_time
+    datetime_of.strftime("%l:%M %p")
+  end
+
+  def format_datetime
+    "#{format_date} at #{format_time}"
+  end
 end
