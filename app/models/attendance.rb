2c6af9814d75446cc24affac150e808643b87e33
Hugo <huwgoma@gmail.com>
2022-09-19 16:54:30 -0400

    Write custom validation method for Attendances - If the event has already passed, do not allow further signups

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,5 +4,13 @@ class Attendance < ApplicationRecord
 
   validates :attendee_id, uniqueness: { scope: :event_id, message: "You are already attending" }
   validates :attendee_id, comparison: { other_than: Proc.new { |a| a.event.host.id }, message: "You are the host" }
+  validate :event_window_closed
+  
+  private 
 
+  def event_window_closed
+    if DateTime.now > event.datetime_of
+      errors.add(:event, :already_happened, message: "Already happened")
+    end
+  end
 end
