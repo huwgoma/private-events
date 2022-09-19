896b71b265ea5d791010e808736f77f396a9edd3
Hugo <huwgoma@gmail.com>
2022-09-19 17:18:11 -0400

    Remove old attendance constraint code from AttendancesController

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,5 @@
 class AttendancesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-  #before_action :validate_attendance, only: [:create]
 
   def create
     @attendance = current_user.attendances.build(event_id: params[:id])
@@ -10,21 +9,5 @@ class AttendancesController < ApplicationController
       flash[:alert] = @attendance.errors
     end
     redirect_to event_path(params[:id])
-  end
-
-  private
-
-  def validate_attendance
-    event = Event.find(params[:id])
-    
-    flash[:alert] = case 
-    when event.attendees.include?(current_user)
-      "You are already attending this event!"
-    when event.host == current_user
-      "You are the host of this event!"
-    when DateTime.now > event.datetime_of  
-      "This event has already happened! (Unless you have a time machine)"
-    end
-    redirect_to event if alert
   end
 end
