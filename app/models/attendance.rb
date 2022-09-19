9952c1515cffe5dbc3e398b203864f54daccf2e9
Hugo <huwgoma@gmail.com>
2022-09-19 17:46:35 -0400

    Fix flash[:alert] messages for invalid attendances

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,15 +2,15 @@ class Attendance < ApplicationRecord
   belongs_to :attendee, class_name: "User"
   belongs_to :event
 
-  validates :attendee_id, uniqueness: { scope: :event_id, message: "You are already attending" }
-  validates :attendee_id, comparison: { other_than: Proc.new { |a| a.event.host.id }, message: "You are the host" }
+  validates :attendee_id, uniqueness: { scope: :event_id, message: "You are already attending this event!" }
+  validates :attendee_id, comparison: { other_than: Proc.new { |a| a.event.host.id }, message: "You are the host of this event!" }
   validate :event_window_closed
   
   private 
 
   def event_window_closed
     if DateTime.now > event.datetime_of
-      errors.add(:event, :already_happened, message: "Already happened")
+      errors.add(:event, :already_happened, message: "This event has already happened!")
     end
   end
 end
