c288f3bebaa831574ad15fd1c912d12bde91b089
Hugo <huwgoma@gmail.com>
2022-09-19 13:40:05 -0400

    Write validate_attendance method - run before an Attendance is created; if one of the conditions for an invalid attendance is met, display a customized flash message and redirect to the event page

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,12 +1,26 @@
 class AttendancesController < ApplicationController
-  def create
-    @attendance = current_user.attendances.build(event_id: params[:id])
+  before_action :authenticate_user!, only: [:create]
+  before_action :validate_attendance, only: [:create]
 
-    if @attendance.save
-      # success flash
-    else
-      # error flash - depends on why it failed?
-    end
+  def create
+    current_user.attendances.build(event_id: params[:id]).save
+    flash[:notice] = "You are now attending this event."
     redirect_to event_path(params[:id])
+  end
+
+  private
+
+  def validate_attendance
+    event = Event.find(params[:id])
+    
+    flash[:alert] = case 
+    when event.attendees.include?(current_user)
+      "You are already attending this event!"
+    when event.host == current_user
+      "You are the host of this event!"
+    when DateTime.now > event.datetime_of  
+      "This event has already happened! (Unless you have a time machine)"
+    end
+    redirect_to event if alert
   end
 end
