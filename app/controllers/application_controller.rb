92498854a6dde011d44450c6caf25f18a0a3dea1
Hugo <huwgoma@gmail.com>
2022-09-20 16:16:53 -0400

    Proof of concept of time zone fix for event times

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_params, if: :devise_controller?
+  around_action :convert_time_zone, if: :current_user
 
   protected
 
   def configure_permitted_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def convert_time_zone(&block)
+    Time.use_zone("Eastern Time (US & Canada)", &block)
   end
 end
