d1250f5f9dd109abcaa43da7bdc41b562166605d
Hugo <huwgoma@gmail.com>
2022-09-20 19:59:27 -0400

    Reorder Attendance validations: event passed -> anti-host -> anti-repeat guests

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,9 +2,9 @@ class Attendance < ApplicationRecord
   belongs_to :attendee, class_name: "User"
   belongs_to :event
 
-  validates :attendee_id, uniqueness: { scope: :event_id, message: "You are already attending this event!" }
-  validates :attendee_id, comparison: { other_than: Proc.new { |a| a.event.host.id }, message: "You are the host of this event!" }
   validate :event_window_closed
+  validates :attendee_id, comparison: { other_than: Proc.new { |a| a.event.host.id }, message: "You are the host of this event!" }
+  validates :attendee_id, uniqueness: { scope: :event_id, message: "You are already attending this event!" }
   
   private 
 
