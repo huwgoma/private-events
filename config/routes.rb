c00df0b3ef0fdab1ac6353f51d4b5513de9bd2ee
Hugo <huwgoma@gmail.com>
2022-09-21 15:55:11 -0400

    Change order of routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   
+  devise_scope :user do 
+    get "users", to: "devise/sessions#new"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "events#index"
-
-  devise_scope :user do 
-    get "users", to: "devise/sessions#new"
-  end
-
-  devise_for :users
   resources :users, only: :show
   resources :events 
   
