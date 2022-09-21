069f74a8a9ca383d0fb92d335c287fe7ec55d213
Hugo <huwgoma@gmail.com>
2022-09-21 14:54:36 -0400

    Rename event date/time formatting methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,17 +13,12 @@ class Event < ApplicationRecord
   scope :past, -> { where("datetime_of < ?", DateTime.now) }
   scope :future, -> { where("datetime_of > ?", DateTime.now) }
 
-  # Format Event Date/Times
-  def format_date
+  # Format Event Date/Time
+  def date_of
     datetime_of.strftime("%B %d, %Y")
   end
 
-  def format_time
+  def time_of
     datetime_of.strftime("%l:%M %p")
   end
-
-  def format_datetime
-    "#{format_date} at #{format_time}"
-  end
-  
 end
