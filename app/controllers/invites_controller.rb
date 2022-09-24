d000ea720655aa00be31fd3f1a9c6bd15f6bc971
Hugo <huwgoma@gmail.com>
2022-09-24 15:41:50 -0400

    Write User::invitable_to - A User class method that filters a User query to only include the users that can be invited to a given event; update Invites#index to use this class method

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,7 +1,7 @@
 class InvitesController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
-    @invitable_users = User.all
+    @invitable_users = User.invitable_to(@event)
   end
 
   def create
