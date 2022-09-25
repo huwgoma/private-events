220f90df3a64979f835c6c52f4d346b047f7dcd3
Hugo <huwgoma@gmail.com>
2022-09-24 20:04:55 -0400

    Outline validations for Invites

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,4 +2,11 @@ class Invite < ApplicationRecord
   belongs_to :inviter, class_name: "User"
   belongs_to :invitee, class_name: "User"
   belongs_to :event
+
+  # Validations
+  # event must be upcoming (joinable)
+
+  # inviter id must = event host
+  # invitee id must be unique to the event id 
+  # invitee id must not be in the event's attendee_ids
 end
