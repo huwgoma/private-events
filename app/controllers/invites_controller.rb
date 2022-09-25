8f96d0fd29af32e250fbdd52f5b74c00c84c5edc
Hugo <huwgoma@gmail.com>
2022-09-25 17:20:51 -0400

    Modify Invites#index view to display list of invited users; modify InvitesController#index to grab the list of invites for that event - temporary

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,7 +1,9 @@
 class InvitesController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
-    @invitable_users = User.invitable_to(@event)
+    #@invitable_users = User.invitable_to(@event)
+    @invitable_users = User.all
+    @invites = Invite.includes(:invitee).where(event: @event)
   end
 
   def create
