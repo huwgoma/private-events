d2e43b423df8077a5369f331532a8f48fd087bb2
Hugo <huwgoma@gmail.com>
2022-09-26 12:12:21 -0400

    Update invites#index form with an Invite model object; write invite_params method in InvitesController to reformat the params that are sent to Invites#create

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,12 +1,20 @@
 class InvitesController < ApplicationController
   def index
     @event = Event.find(params[:event_id])
+    @invite = current_user.sent_invites.new
     #@invitable_users = User.invitable_to(@event)
     @invitable_users = User.all
     @invites = Invite.includes(:invitee).where(event: @event)
   end
 
   def create
+    @event = Event.find(invite_params[:event_id])
+    InvitesCreator.new(invite_params).execute
+  end
 
+  private
+
+  def invite_params
+    params.require(:invite).permit(:event_id, user_ids:[]).merge(inviter_id: current_user.id)
   end
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,10 +3,17 @@ class Invite < ApplicationRecord
   belongs_to :invitee, class_name: "User"
   belongs_to :event
 
-  # Validations
-  # event must be upcoming (joinable)
+  # For batch invites
+  attr_accessor :user_ids
 
-  # inviter id must = event host
-  # invitee id must be unique to the event id 
-  # invitee id must not be in the event's attendee_ids
+  # Event must be upcoming
+  validates_with EventUpcomingValidator
+  # Inviter must be the event host
+  validates :inviter_id, comparison: { equal_to: Proc.new { |i| i.event.host.id }, message: "You must be the host of this event to invite users!" }
+  # Invitee must not be the event host
+  validates :invitee_id, comparison: { other_than: Proc.new { |i| i.event.host.id }, message: "You are the host of this event!" }
+  # Invitee must be unique to the event
+  validates :invitee_id, uniqueness: { scope: :event_id, message: "This user has already been invited to this event!" }
+  # Invitee must not be attending already
+  validates :invitee_id, exclusion: { in: -> (i) { i.event.attendee_ids }, message: "This user is already attending this event!" }
 end
